6dba70b1178b2e7881dbe540cf61f23ec8e15da2
jdallagnol <joao.dallagnol1@gmail.com>
2023-08-16 10:51:08 -0300

    Created CloudKitProtocol

--- Sources/CloudKitPackage/Implementation/CloudKitImplementation.swift
+++ Sources/CloudKitPackage/Implementation/CloudKitImplementation.swift
@@ -1,6 +1,7 @@
 import CloudKit
 
-public struct CloudKitImplementation {
+public struct CloudKitImplementation: CloudKitProtocol {
+    
     public private(set) var text = "Hello, World!"
     private let publicDatabase = CKContainer(identifier: "iCould.br.org.eldorado.monalisa-ck-spike").publicCloudDatabase
     
---
+++ Sources/CloudKitPackage/Interface/Protocol/CloudKitProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by João Dall Agnol on 16/08/23.
+//
+
+import Foundation
+
+public protocol CloudKitProtocol {
+    func createRecord()
+    func readRecord()
+    func updateRecord()
+    func removeRecord()
+}
